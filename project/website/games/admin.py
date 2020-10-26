537cf6e6d9b78d3ce46d19efe7eade3a084c55cb
Nihisil <alexey@nihisil.com>
2020-10-26 13:02:19 +0800

    Improve latest games page admin interface

--- project/website/games/admin.py
+++ project/website/games/admin.py
@@ -7,15 +7,22 @@ from website.games.models import Game
 
 class GameAdmin(admin.ModelAdmin):
     search_fields = ['external_id']
-    ordering = ['-created_at']
-    list_filter = ['status']
-    list_display = ['player', 'player_position', 'scores', 'status', 'created_at', 'action']
+    ordering = ['-updated_at']
+    list_filter = ['player__username', 'status']
+    list_display = ['id', 'player_link', 'place', 'scores', 'status', 'created_at', 'updated_at', 'action']
+
+    def place(self, obj):
+        return obj.player_position
 
     def action(self, obj):
         if obj.status == Game.STARTED:
             url = reverse('manually_load_results', kwargs={'game_id': obj.id})
             return format_html(f'<a href="{url}">Load results</a>')
         return ''
+
+    def player_link(self, obj):
+        url = reverse('player_statistics', kwargs={'player_name': obj.player.username})
+        return format_html(f'<a href="{url}" target="_blank">{obj.player.username}</a>')
 
 
 admin.site.register(Game, GameAdmin)
